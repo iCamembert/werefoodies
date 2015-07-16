6ef4bd66bba67ee707a1b140e06d54b4a0b7beba
iCamembert <guillaume.ducluzeau@gmail.com>
2015-07-16 15:46:30 +0200

    design problem for chrome

--- resources/views/partials/_login-modal.blade.php
+++ resources/views/partials/_login-modal.blade.php
@@ -3,7 +3,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+                <h4 class="modal-title" id="myModalLaabel">Modal title</h4>
             </div>
             <div class="modal-body">
                 <div class="container-fluid" style="">
@@ -23,7 +23,7 @@
                                         </div>
                                     @endif
 
-                                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/login') }}">
+                                    <form class="form-horizontal" method="POST" action="{{ url('/auth/login') }}">
                                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
                                         <div class="form-group">
