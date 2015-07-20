f6a6952c3ac4394836e6f54c941e6d21a4573cf7
iCamembert <guillaume.ducluzeau@gmail.com>
2015-07-20 17:20:28 +0200

    dish picture update fix

--- resources/views/users/_chef.blade.php
+++ resources/views/users/_chef.blade.php
@@ -218,7 +218,7 @@
                                                     <strong>SEE</strong> MORE
                                                 </span>
                                             </a>
-                                            <img class="img-responsive" src="{{ asset('/userdata/' . $user->id . '/dishes/' . $dish->id . '/picture_md.jpg') }}" width="260" height="260" alt="">
+                                            <img class="img-responsive" src="{{ asset('/userdata/' . $user->id . '/dishes/' . $dish->id . '/picture_md.jpg') }}" width="200" height="200" alt="">
                                         </figure>
                                         <div class="item-box-desc">
                                             <h4>{{ $dish->name }}</h4>
@@ -237,7 +237,7 @@
                                                     <strong>SEE</strong> MORE
                                                 </span>
                                             </a>
-                                            <img class="img-responsive" src="{{ asset('/userdata/' . $user->id . '/dishes/' . $dish->id . '/picture_md.jpg') }}" width="260" height="260" alt="">
+                                            <img class="img-responsive" src="{{ asset('/userdata/' . $user->id . '/dishes/' . $dish->id . '/picture_md.jpg') }}" width="200" height="200" alt="">
                                         </figure>
                                         <div class="item-box-desc">
                                             <h4>{{ $dish->name }}</h4>
