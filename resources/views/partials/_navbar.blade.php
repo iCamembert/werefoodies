ed98055a00a9340c2db1d0c6f04ffbb4f6357f06
iCamembert <guillaume.ducluzeau@gmail.com>
2015-07-21 23:56:17 +0200

    cart attempt

--- resources/views/partials/_navbar.blade.php
+++ resources/views/partials/_navbar.blade.php
@@ -150,7 +150,9 @@
                                 <p><i class="fa fa-warning"></i> Your hungry cart currently contains:</p>
                                 {{ $content = Cart::content() }}
                                 @foreach ($content as $item)
-                                          <div class="inline-block">
+                                    <a class="item" href="{{ action('DishesController@show', array('dishes' => $item->dish, 'isBeingOrdered' => 0)) }}">
+                                        <img class="pull-left" src="{{ asset('/userdata/' . $item->dish->user_id . '/dishes/' . $item->dish->id . '/picture_sm.jpg') }}" width="40" alt="Dish Picture" />
+                                        <div class="inline-block">
                                             <span class="title">{{ $item->name }}</span>
                                             <span class="price">{{ $item->qty }} &times; ${{ $item->price }} </span>
                                         </div>
