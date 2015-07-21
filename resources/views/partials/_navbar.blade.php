7da304d976ee1069629768fe7455ae66f79786ce
iCamembert <guillaume.ducluzeau@gmail.com>
2015-07-22 00:28:07 +0200

    cart attempt

--- resources/views/partials/_navbar.blade.php
+++ resources/views/partials/_navbar.blade.php
@@ -151,7 +151,7 @@
                                 {{ $content = Cart::content() }}
                                 @foreach ($content as $item)
                                     <a class="item" href="">
-                                        <img class="pull-left" src="{{ asset('/userdata/' . $item->user_id . '/dishes/' . $item->id . '/picture_sm.jpg') }}" width="40" alt="Dish Picture" />
+                                        <img class="pull-left" src="{{ asset('/userdata/' . 1 . '/dishes/' . $item->id . '/picture_sm.jpg') }}" width="40" alt="Dish Picture" />
                                         <div class="inline-block">
                                             <span class="title">{{ $item->name }}</span>
                                             <span class="price">{{ $item->qty }} &times; ${{ $item->price }} </span>
