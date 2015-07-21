4e7c5bd279f5b4d6bd98ebef47293557568c5290
iCamembert <guillaume.ducluzeau@gmail.com>
2015-07-22 00:17:13 +0200

    cart attempt

--- app/Http/Controllers/DishesController.php
+++ app/Http/Controllers/DishesController.php
@@ -165,7 +165,7 @@ class DishesController extends Controller {
 	{
 		$quantity = Input::get('quantity');
 
-		Cart::add($dish->id, $dish->name, $quantity, $dish->price, array('chefId' => $dish->user_id, 'dish' => $dish));
+		Cart::add($dish->id, $dish->name, $quantity, $dish->price, array('chefId' => $dish->user_id));
 
 		return redirect()->back()->with('flash_message', 'Your cart has been updated!');
 	}
