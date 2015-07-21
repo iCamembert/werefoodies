1d698b52cfae8f0d69d3baa0b0441ff84ef9b163
iCamembert <guillaume.ducluzeau@gmail.com>
2015-07-21 22:53:34 +0200

    cart attempt

--- resources/views/dishes/show.blade.php
+++ resources/views/dishes/show.blade.php
@@ -62,7 +62,7 @@
                             @else
                                 <div id="dishQuantityBlock" class="row">
                                     <div class="form-group center-block" style="display: flex; align-items: center;">
-                                        {!! Form::open(['route' => '/add-to-cart']) !!}
+                                        {!! Form::open(['url' => '/add-to-cart']) !!}
                                             <input type="hidden" name="dishId" value="{{ $dish->id }}">
                                             <input type="hidden" name="dishName" value="{{ $dish->name }}">
                                             <div class="col-md-3">
