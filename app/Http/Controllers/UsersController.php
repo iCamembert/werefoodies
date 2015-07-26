6bb38aff15696135e817157917c1594fcbb75bfb
iCamembert <guillaume.ducluzeau@gmail.com>
2015-07-26 17:44:59 +0200

    orders system completion

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -62,7 +62,7 @@ class UsersController extends Controller {
 		   	}
         }
 
-        $clientOrders->unique('order_id')->sortByDesc('updated_at')->toArray();
+        $clientOrders = $clientOrders->unique('order_id')->sortByDesc('updated_at')->toArray();
 
         return view('users.edit', compact('user', 'clientOrders'));
 	}
