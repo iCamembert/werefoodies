1d54ca736716d334fb70293f60ef9c9c4896c392
iCamembert <guillaume.ducluzeau@gmail.com>
2015-07-26 17:13:20 +0200

    orders system completion

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,7 +10,7 @@ use Request;
 use App\Http\Requests\UserRequest;
 use Illuminate\Support\Facades\File;
 use Intervention\Image\Facades\Image;
-use Illuminate\Support\Collection;
+use Illuminate\Database\Eloquent\Collection;
 
 class UsersController extends Controller {
 
