26fcb742493574d0a352a7dede2fc4f87e9b6ae5
iCamembert <guillaume.ducluzeau@gmail.com>
2015-07-26 16:46:06 +0200

    orders system completion

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -45,7 +45,7 @@ class UsersController extends Controller {
         	$clientOrders->add($dish->orders);
         }
 
-        $clientOrders->sortByDesc('updated_at')->get();
+        dd($clientOrders->sortByDesc('updated_at')->get());
 
         return view('users.show', compact('user', 'clientOrders'));
 	}
