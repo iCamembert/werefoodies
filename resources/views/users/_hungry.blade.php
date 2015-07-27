17d20869033f44d668f22181b706f1009c58f7e7
iCamembert <guillaume.ducluzeau@gmail.com>
2015-07-27 10:06:22 +0200

    orders system completion

--- resources/views/users/_hungry.blade.php
+++ resources/views/users/_hungry.blade.php
@@ -34,7 +34,6 @@
                                                         <li><a href="action('DishesController@show', array('dishes' => $orderedDish->id))" title="See Dish">{{ $orderedDish->name }}: {{ orderedDish->quantity }}</a></td></li>
                                                     @endforeach
                                                 </ul>
-                                            <td class="text-center" style="vertical-align: middle;"><a href="action('UsersController@show', array('users' => $order->dishes->first()->user->id))" title="See Profile"></a></td>
                                             <td class="text-center" style="vertical-align: middle;">{{ $order->price }}</td>
                                             <td class="text-center" style="vertical-align: middle;">{{ $order->served_at }}</td>
                                             <td class="text-center" style="vertical-align: middle;">
