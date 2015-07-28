9f5dc0da9c8a94bd63dbdb78f04668e29c73d45e
iCamembert <guillaume.ducluzeau@gmail.com>
2015-07-28 18:41:43 +0200

    review system with multiple dishes rating

--- resources/lang/en/strings.php
+++ resources/lang/en/strings.php
@@ -30,6 +30,17 @@ return [
     "navAllRightsReserved" => "ALL RIGHTS RESERVED.",
     "navBackToTop" => "BACK TO TOP",
 
+    // ******** Bread Crumbs ********
+
+    "breadHome" => "Home",
+    "breadSignIn" => "Sign In",
+    "breadSignUp" => "Sign Up",
+    "breadMyAccount" => "My Account",
+    "breadAboutUser" => "About",
+    "breadMyCart" => "My Cart",
+    "breadPlaceOrder" => "Place Order",
+    "breadEditOrder" => "Edit Order",
+
     // ******** Home Page ********
 
     "homeSubtitle" => "The ultimate food network!",
@@ -52,6 +63,84 @@ return [
     "homeBestDishes3" => "Order",
     "homeSubscribe1" => "Subscrive <strong>now</strong> to receive our <strong>tasty</strong> newsletter!",
     "homeSubscribe2" => "Type your best E-mail Address",
-    "homeSubscribe3" => "E-mail Address"
+    "homeSubscribe3" => "E-mail Address",
+
+    // ******** Sign In Page ********
+
+    "signInLogin1" => "Let's <strong>Eat</strong>!",
+    "signInLogin2" => "E-Mail Address",
+    "signInLogin3" => "Password",
+    "signInLogin4" => "Remember Me",
+    "signInLogin5" => "Sign In",
+    "signInForgot1" => "Forgot <strong>Password</strong>?",
+    "signInForgot2" => "That's okay, your food isn't gonna fly away! Just follow this quick step to retrieve your password:",
+    "signInForgot3" => "<strong>Type your E-mail Address</strong>",
+    "signInForgot4" => "E-mail Address",
+    "signInForgot5" => "Send Reset Link",
+    "signInNoAccountYet" => "Not a member yet? <a href=\"{{ url('/auth/register') }}\">Click here to register</a>, it's quick and free!",
+
+    // ******** Sign Up Page ********
+
+    "signUpRegister1" => "Create <strong>Account</strong>",
+    "signUpRegister1" => "Name",
+    "signUpRegister1" => "E-mail Address",
+    "signUpRegister1" => "Password",
+    "signUpRegister1" => "Confirm Password",
+    "signUpRegister1" => "Sign Up",
+    "signUpWhy1" => "Why Register?",
+    "signUpWhy2" => "<strong>Because you can! :)</strong>",
+    "signUpWhy3" => "Free registration will allow you to:",
+    "signUpWhy4" => "Search for & order awesome dishes made by awesome chefs in your area!",
+    "signUpWhy5" => "Rate, review, & require dishes, participate to the social side of Food!",
+    "signUpWhy6" => "Become a chef yourself, make your creations visible, share your passion & become rich!",
+    "signUpWhy7" => "Play to our games, participate to our contests & events, all related to Food!",
+    "signUpWhy8" => "Eat!",
+    "signUpAlreadyAccount" => "Already a member? <a href=\"{{ url('/auth/login') }}\">Click here to sign in</a>!",
+    "signUpContact1" => "<strong>Contact our support team!</strong>",
+    "signUpContact2" => "Don't hesitate, <a href=\"\">click here if you need help</a>!",
+
+    // ******** Profile Page ********
+
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+
+    // ******** Dish Page ********
+
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+
+    // ******** Cart Page ********
+
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+
+    // ******** Order Page ********
+
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
 
 ];
--- resources/lang/fr/strings.php
+++ resources/lang/fr/strings.php
@@ -13,11 +13,134 @@ return [
     |
     */
 
-    "homeSubtitle" => "Le réseau ultime pour bien manger!",
-    "todayBestDishes" => "Les Plats du Jour",
-    "orderThemNow" => "Commandez-les Maintenant",
-    "chefsOfTheWeek" => "Les Chefs de la Semaine",
-    "orderFreshDishesNow" => "Commandez dès maintenant de bons petits plats",
-    "welcomeToEatMyThings" => "<strong>Bienvenue</strong> sur Eat My Things"
+    // ******** Navigation ********
+
+    "navAbout" => "Qui Sommes Nous?",
+    "navContact" => "Contactez-Nous",
+    "navSignIn" => "Se Connecter",
+    "navSignOut" => "Se Déconnecter",
+    "navMenuTitle" => "Accueil",
+    "navMenuItem1" => "Comment ça marche?",
+    "navMenuItem2" => "Astuces",
+    "navSearch" => "Cherchez sur le site!",
+    "navCartEmpty" => "You currently haven't got anything in your hungry cart!",
+    "navCartNotEmpty" => "Your hungry cart currently contains:",
+    "navCartButton1" => "View Cart",
+    "navCartButton2" => "Checkout",
+    "navAllRightsReserved" => "ALL RIGHTS RESERVED.",
+    "navBackToTop" => "BACK TO TOP",
+
+    // ******** Bread Crumbs ********
+
+    "breadHome" => "Home",
+    "breadSignIn" => "Sign In",
+    "breadSignUp" => "Sign Up",
+    "breadMyAccount" => "My Account",
+    "breadAboutUser" => "About",
+    "breadMyCart" => "My Cart",
+    "breadPlaceOrder" => "Place Order",
+    "breadEditOrder" => "Edit Order",
+
+    // ******** Home Page ********
+
+    "homeSubtitle" => "The ultimate food network!",
+    "homeCarousel1" => "Today's Best Dishes",
+    "homeCarousel2" => "Order Them Now",
+    "homeCarousel3" => "Chefs of the Week",
+    "homeCarousel4" => "Order Fresh Dishes Now",
+    "homeWelcome1" => "<strong>Welcome</strong> to Eat My Things",
+    "homeWelcome2" => "Dishes Made By People, For People!",
+    "homeSearch1" => "Where do you want to eat?",
+    "homeSearch2" => "Find a chef near you",
+    "homeSearch3" =>"Type in your location (e.g. 'Paris, France')",
+    "homeSearch4" => "Advanced Search",
+    "homeSearch5" => "By Name:",
+    "homeSearch6" => "Rating Above:",
+    "homeSearch7" => "Price Below:",
+    "homeSearch8" => "Cancel",
+    "homeBestDishes1" => "Hungry? Come and check out the best dishes cooked by the best chefs in your area.",
+    "homeBestDishes2" => "<strong>See</strong> More",
+    "homeBestDishes3" => "Order",
+    "homeSubscribe1" => "Subscrive <strong>now</strong> to receive our <strong>tasty</strong> newsletter!",
+    "homeSubscribe2" => "Type your best E-mail Address",
+    "homeSubscribe3" => "E-mail Address",
+
+    // ******** Sign In Page ********
+
+    "signInLogin1" => "Let's <strong>Eat</strong>!",
+    "signInLogin2" => "E-Mail Address",
+    "signInLogin3" => "Password",
+    "signInLogin4" => "Remember Me",
+    "signInLogin5" => "Sign In",
+    "signInForgot1" => "Forgot <strong>Password</strong>?",
+    "signInForgot2" => "That's okay, your food isn't gonna fly away! Just follow this quick step to retrieve your password:",
+    "signInForgot3" => "<strong>Type your E-mail Address</strong>",
+    "signInForgot4" => "E-mail Address",
+    "signInForgot5" => "Send Reset Link",
+    "signInNoAccountYet" => "Not a member yet? <a href=\"{{ url('/auth/register') }}\">Click here to register</a>, it's quick and free!",
+
+    // ******** Sign Up Page ********
+
+    "signUpRegister1" => "Create <strong>Account</strong>",
+    "signUpRegister1" => "Name",
+    "signUpRegister1" => "E-mail Address",
+    "signUpRegister1" => "Password",
+    "signUpRegister1" => "Confirm Password",
+    "signUpRegister1" => "Sign Up",
+    "signUpWhy1" => "Why Register?",
+    "signUpWhy2" => "<strong>Because you can! :)</strong>",
+    "signUpWhy3" => "Free registration will allow you to:",
+    "signUpWhy4" => "Search for & order awesome dishes made by awesome chefs in your area!",
+    "signUpWhy5" => "Rate, review, & require dishes, participate to the social side of Food!",
+    "signUpWhy6" => "Become a chef yourself, make your creations visible, share your passion & become rich!",
+    "signUpWhy7" => "Play to our games, participate to our contests & events, all related to Food!",
+    "signUpWhy8" => "Eat!",
+    "signUpAlreadyAccount" => "Already a member? <a href=\"{{ url('/auth/login') }}\">Click here to sign in</a>!",
+    "signUpContact1" => "<strong>Contact our support team!</strong>",
+    "signUpContact2" => "Don't hesitate, <a href=\"\">click here if you need help</a>!",
+
+    // ******** Profile Page ********
+
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+
+    // ******** Dish Page ********
+
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+
+    // ******** Cart Page ********
+
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+
+    // ******** Order Page ********
+
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
+    "profileName" => "Name",
 
 ];
