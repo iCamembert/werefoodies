4320d39a54a8cae26175b06cc4d33573dbffd752
iCamembert <guillaume.ducluzeau@gmail.com>
2015-07-29 12:19:43 +0200

    languages

--- resources/views/partials/_navbar.blade.php
+++ resources/views/partials/_navbar.blade.php
@@ -12,11 +12,7 @@
 
             <ul class="dropdown-menu">
                 @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
-                    <li>
-                        <a rel="alternate" hreflang="{{$localeCode}}" href="{{LaravelLocalization::getLocalizedURL($localeCode) }}">
-                            <img src="{{ asset('img/flags/{{$localeCode}}.png') }}" width="16" height="11" alt="{{$localeCode}} Language" /> [{{$localeCode}}] {{ $properties['native'] }}
-                        </a>
-                    </li>
+                    
                 @endforeach
             </ul>
         </div>
