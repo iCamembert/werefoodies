ffd8ebb84aa4ea3a0b8c4e62391aa4384b8ea766
iCamembert <guillaume.ducluzeau@gmail.com>
2015-08-09 01:40:29 +0200

    test seeding

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -35,73 +35,74 @@ class CreateUsersTable extends Migration {
             $table->string('google_place_id');
 			$table->timestamps();
 		});
+
+		User::create([
+			'name' => 'Guillaume',
+			'email' => 'guillaume@gmail.com',
+			'password' => Hash::make('123456')
+		]);
+
+		User::create([
+			'name' => 'Norah',
+			'email' => 'norah@gmail.com',
+			'password' => Hash::make('123456')
+		]);
+
+		User::create([
+			'name' => 'Camille',
+			'email' => 'camille@gmail.com',
+			'password' => Hash::make('123456')
+		]);
+
+		User::create([
+			'name' => 'Henry',
+			'email' => 'henry@gmail.com',
+			'password' => Hash::make('123456')
+		]);
+
+		User::create([
+			'name' => 'Yura',
+			'email' => 'yura@gmail.com',
+			'password' => Hash::make('123456')
+		]);
+
+		User::create([
+			'name' => 'Yannick',
+			'email' => 'yannick@gmail.com',
+			'password' => Hash::make('123456')
+		]);
+
+		User::create([
+			'name' => 'Nady',
+			'email' => 'nady@gmail.com',
+			'password' => Hash::make('123456')
+		]);
+
+		User::create([
+			'name' => 'ducj',
+			'email' => 'ducj@gmail.com',
+			'password' => Hash::make('123456')
+		]);
+
+		User::create([
+			'name' => 'Victor',
+			'email' => 'victor@gmail.com',
+			'password' => Hash::make('123456')
+		]);
+
+		User::create([
+			'name' => 'nicolaS',
+			'email' => 'nicolas@gmail.com',
+			'password' => Hash::make('123456')
+		]);
+
+		User::create([
+			'name' => 'bBob',
+			'email' => 'bbob@gmail.com',
+			'password' => Hash::make('123456')
+		]);
+		
 	}
-
-	User::create([
-		'name' => 'Guillaume',
-		'email' => 'guillaume@gmail.com',
-		'password' => Hash::make('123456')
-	]);
-
-	User::create([
-		'name' => 'Norah',
-		'email' => 'norah@gmail.com',
-		'password' => Hash::make('123456')
-	]);
-
-	User::create([
-		'name' => 'Camille',
-		'email' => 'camille@gmail.com',
-		'password' => Hash::make('123456')
-	]);
-
-	User::create([
-		'name' => 'Henry',
-		'email' => 'henry@gmail.com',
-		'password' => Hash::make('123456')
-	]);
-
-	User::create([
-		'name' => 'Yura',
-		'email' => 'yura@gmail.com',
-		'password' => Hash::make('123456')
-	]);
-
-	User::create([
-		'name' => 'Yannick',
-		'email' => 'yannick@gmail.com',
-		'password' => Hash::make('123456')
-	]);
-
-	User::create([
-		'name' => 'Nady',
-		'email' => 'nady@gmail.com',
-		'password' => Hash::make('123456')
-	]);
-
-	User::create([
-		'name' => 'ducj',
-		'email' => 'ducj@gmail.com',
-		'password' => Hash::make('123456')
-	]);
-
-	User::create([
-		'name' => 'Victor',
-		'email' => 'victor@gmail.com',
-		'password' => Hash::make('123456')
-	]);
-
-	User::create([
-		'name' => 'nicolaS',
-		'email' => 'nicolas@gmail.com',
-		'password' => Hash::make('123456')
-	]);
-
-	User::create([
-		'name' => 'bBob',
-		'email' => 'bbob@gmail.com',
-		'password' => Hash::make('123456')
-	]);
 
 	/**
 	 * Reverse the migrations.
