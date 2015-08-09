818f9d5f8026191ae1d0f0585ec6dc11cfc1c38f
iCamembert <guillaume.ducluzeau@gmail.com>
2015-08-10 01:10:16 +0200

    map again

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -121,7 +121,9 @@ class CreateUsersTable extends Migration {
 	 */
 	public function down()
 	{
+		\DB::statement('SET FOREIGN_KEY_CHECKS = 0');
 		Schema::drop('users');
+		\DB::statement('SET FOREIGN_KEY_CHECKS = 1');
 	}
 
 }
