400dad20916d2484955204cdfc569f74f2ce4743
iCamembert <guillaume.ducluzeau@gmail.com>
2015-08-11 15:16:28 +0200

    map again

--- database/migrations/2015_05_18_154503_create_dishes_table.php
+++ database/migrations/2015_05_18_154503_create_dishes_table.php
@@ -32,53 +32,46 @@ class CreateDishesTable extends Migration {
 		});
 
 		User::find(1)->dishes()->create([
-			'user_id' => 1,
 			'price' => 4.0,
 			'name' => 'Mousse au chocolat',
 			'rating' => 2.2
 		]);
 
-		/*Dish::create([
-			'user_id' => 1,
+		User::find(1)->dishes()->create([
 			'price' => 3.0,
 			'name' => 'Tarte au citron meringuée',
 			'rating' => 4.2
 		]);
 
-		Dish::create([
-			'user_id' => 2,
+		User::find(2)->dishes()->create([
 			'price' => 5.0,
 			'name' => 'Risotto au poulet',
 			'rating' => 3.2
 		]);
 
-		Dish::create([
-			'user_id' => 3,
+		User::find(3)->dishes()->create([
 			'price' => 7.0,
 			'name' => 'Filet de dorade sauce citronnelle',
 			'rating' => 4.1
 		]);
 
-		Dish::create([
-			'user_id' => 3,
+		User::find(3)->dishes()->create([
 			'price' => 6.0,
 			'name' => 'Pizza maison',
 			'rating' => 4.2
 		]);
 
-		Dish::create([
-			'user_id' => 3,
+		User::find(3)->dishes()->create([
 			'price' => 5.0,
 			'name' => 'Raviolis au calmar',
 			'rating' => 4.8
 		]);
 
-		Dish::create([
-			'user_id' => 4,
+		User::find(4)->dishes()->create([
 			'price' => 4.0,
 			'name' => 'Soupe aux épinards',
 			'rating' => 4.0
-		]);*/
+		]);
 	}
 
 	/**
