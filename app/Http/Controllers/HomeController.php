58aa358bc913c547f1e8437c51479664ee972d32
iCamembert <guillaume.ducluzeau@gmail.com>
2015-09-12 18:23:57 +0200

    search

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -67,7 +67,7 @@ class HomeController extends Controller {
 
 	public function search($googlePlaceId)
 	{
-        $dishesForMap = Dish::select('dishes.*', 'users.address_google_place_id')->leftJoin('users', 'dishes.user_id', '=', 'users.id')->where('users.city_google_place_id', '=', $googlePlaceId)->groupBy('dishes.user_id')->orderBy('dishes.rating', 'desc')->get();
+        $dishesForMap = Dish::orderBy('rating', 'desc')->groupBy('user_id')->get();//Dish::select('dishes.*', 'users.address_google_place_id')->leftJoin('users', 'dishes.user_id', '=', 'users.id')->where('users.city_google_place_id', '=', $googlePlaceId)->groupBy('dishes.user_id')->orderBy('dishes.rating', 'desc')->get();
 		
 		return view('search', compact('dishesForMap'));
 	}
